131984b0866263054de971da4c6ded02220589b8
Patrik Wallin Hybelius <p.wallin.hybelius@gmail.com>
2026-08-01 15:53:53 +0200

    Add Sine/Cosine type II C++ implementations

---
+++ src/transforms/fft.h
@@ -0,0 +1,20 @@
+#include <cstdint>
+#include <vector>
+
+namespace numeric::transforms
+{
+
+enum class SIN_COS_TRF_TYPE
+{
+    I,
+    II,
+    III,
+    IV
+};
+
+std::vector<float> discrete_cosine_transform(const std::span<const float> input, SIN_COS_TRF_TYPE type);
+std::vector<float> discrete_cosine_transform_pow_2(const std::span<const float> input, SIN_COS_TRF_TYPE type);
+
+std::vector<float> discrete_sine_transform(const std::span<const float> input, SIN_COS_TRF_TYPE type);
+
+} // namespace numeric::transforms
